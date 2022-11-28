c20ebe315b45bb7540d09e879b792f302e4bbc0c
emmanuel-olateju <olatejuemmanuel@gmail.com>
2022-11-28 11:28:31 +0100

    wavelet method fixed using scipy

--- wavelet_methods.py
+++ wavelet_methods.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pywt
+import scipy.signal as signal
 
 def wavelet_coef_freq(signal, Fs=250, scale=(1,11), waveletname = 'cmor',method_='conv'):
   Ts=1/Fs
@@ -27,3 +28,24 @@ def D3_wavelet_coef_freq(X, Fs=250, scale=(1,11), waveletname = 'cmor',method_='
       coeffs[i,:,:] = D2_wavelet_coef_freq(X[i,:,:], Fs, scale, waveletname, method_)
 
   return coeffs
+
+def sc_wavelet_coef_freq(X,width=np.arange(1,31)):
+  return signal.cwt(X,signal.ricker,width)
+
+def sc_D2_wavelet_coef_freq(X,width=np.arange(1,31)):
+  assert X.ndim==2
+  cwtmatr = np.empty((X.shape[0],len(width),X.shape[1]))
+
+  for ch in range(X.shape[0]):
+    cwtmatr[ch,:,:] = sc_wavelet_coef_freq(X[ch,:],width)
+  
+  return cwtmatr
+
+def sc_D3_wavelet_coef_freq(X,width=np.arange(1,31)):
+  assert X.ndim==3
+  cwtmatr = np.empty((X.shape[0],X.shape[1],len(width),X.shape[2]))
+
+  for ch in range(X.shape[0]):
+    cwtmatr[ch,:,:,:] = sc_D2_wavelet_coef_freq(X[ch,:,:],width)
+
+  return cwtmatr
